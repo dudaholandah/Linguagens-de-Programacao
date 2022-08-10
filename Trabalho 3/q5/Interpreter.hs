8c15734f6142df9b29f43d99aeb8884ad9dfb02f
dudaholandah <duda.mholanda@gmail.com>
2022-08-10 19:21:04 -0300

    add q4 e q5

--- Trabalho 3/q5/Interpreter.hs
+++ Trabalho 3/q5/Interpreter.hs
@@ -2,17 +2,9 @@ module Root.Questoes.Q5.Interpreter where
 
 import Root.Questoes.Q5.AbsLI
 
--- As definições deste arquivo são as mínimas para compilar os testes.
--- Você deverá completar todo o restante do código.
--- Dica: se você fez os exercícios anteriores, boa parte do código
--- pode ser reutilizado neste exercício.
-
 type RContext = [(String, Valor)]
 
 type ErrorMessage = String
-
-executeP :: RContext -> Program -> Either ErrorMessage RContext
-executeP = undefined 
 
 data Valor
   = ValorStr String
@@ -32,3 +24,98 @@ i (ValorInt vint) = vint
 
 b :: Valor -> Bool
 b (ValorBool vbool) = vbool
+
+getStr :: Ident -> String
+getStr (Ident s) = s
+
+lookupN :: RContext -> String -> Valor
+lookupN ((i,v):cs) s
+   | i == s = v
+   | otherwise = lookupN cs s
+
+update :: RContext -> String -> Valor -> RContext
+update [] s v = [(s, v)]
+update ((i, v) : cs) s nv
+  | i == s = (i, nv) : cs
+  | otherwise = (i, v) : update cs s nv 
+
+
+executeP :: RContext -> Program -> Either ErrorMessage RContext
+executeP context (Prog stm) = execute context stm
+
+
+execute :: RContext -> Stm -> Either ErrorMessage RContext
+execute context x = case x of
+  SAss id exp -> case eval context exp of
+                        Right vexp -> Right ( update context (getStr id) vexp )
+                        Left msg -> Left msg
+  SBlock [] -> Right context
+  SBlock (s : stms) -> case execute context s of
+                        Right cont -> execute cont (SBlock stms)
+                        Left msg -> Left msg
+  SWhile exp stm -> case eval context exp of
+                        Right ( ValorInt vexp ) -> if (vexp == 0)
+                                        then Right context
+                                        else case execute context stm of
+                                                Right cont -> execute cont (SWhile exp stm)
+                                                Left msg -> Left msg
+                        Left msg -> Left msg
+  SdoWhile stm exp -> case execute context stm of
+                        Right cont -> execute cont (SWhile exp stm)
+                        Left msg -> Left msg
+  STry [] stmsC stmsF -> execute context ( SBlock stmsF )                      
+  STry (s : stmsT) stmsC stmsF -> case execute context s of
+                                        Right cont -> execute cont ( STry stmsT stmsC stmsF )
+                                        Left _ -> case execute context ( SBlock stmsC ) of
+                                                        Right nCont -> execute nCont ( SBlock stmsF )
+                                                        Left msg -> Left msg
+
+
+eval :: RContext -> Exp -> Either ErrorMessage Valor
+eval context x = case x of
+  EAdd exp0 exp -> case eval context exp0 of
+                        Right ( ValorInt vexp0 ) -> case eval context exp of
+                                        Right ( ValorInt vexp ) -> Right ( ValorInt (vexp0 + vexp) )
+                                        Left msg -> Left msg
+                        Left msg -> Left msg
+  ESub exp0 exp -> case eval context exp0 of
+                        Right ( ValorInt vexp0 ) -> case eval context exp of
+                                        Right ( ValorInt vexp ) -> Right ( ValorInt (vexp0 - vexp) )
+                                        Left msg -> Left msg
+                        Left msg -> Left msg                     
+  EMul exp0 exp -> case eval context exp0 of
+                        Right ( ValorInt vexp0 ) -> case eval context exp of
+                                        Right ( ValorInt vexp ) -> Right ( ValorInt (vexp0 * vexp) )
+                                        Left msg -> Left msg
+                        Left msg -> Left msg
+  EDiv exp0 exp -> case eval context exp0 of
+                        Right ( ValorInt vexp0 ) -> case eval context exp of
+                                        Right ( ValorInt vexp ) -> if (vexp == 0)
+                                                        then Left ("divisao por 0")
+                                                        else Right ( ValorInt (vexp0 `div` vexp) )
+                                        Left msg -> Left msg
+                        Left msg -> Left msg
+  ECon exp0 exp -> case eval context exp0 of
+                        Right ( ValorStr vexp0 ) -> case eval context exp of
+                                        Right ( ValorStr vexp ) -> Right ( ValorStr (vexp0 ++ vexp) )
+                                        Left msg -> Left msg
+                        Left msg -> Left msg                                        
+  EInt n -> Right (ValorInt n)
+  EVar id -> Right ( lookupN context (getStr id) ) 
+  EStr str -> Right (ValorStr str)
+  EOr exp0 exp -> case eval context exp0 of
+                        Right ( ValorBool vexp0 ) -> case eval context exp of
+                                        Right ( ValorBool vexp ) -> Right ( ValorBool (vexp0 || vexp) )
+                                        Left msg -> Left msg
+                        Left msg -> Left msg
+  EAnd exp0 exp -> case eval context exp0 of
+                        Right ( ValorBool vexp0 ) -> case eval context exp of
+                                        Right ( ValorBool vexp ) -> Right ( ValorBool (vexp0 && vexp) )
+                                        Left msg -> Left msg
+                        Left msg -> Left msg
+  ENot exp -> case eval context exp of
+                        Right ( ValorBool vexp ) -> Right (ValorBool (not vexp))
+                        Left msg -> Left msg
+  ETrue -> Right (ValorBool True)  
+  EFalse -> Right (ValorBool False) 
+ 
