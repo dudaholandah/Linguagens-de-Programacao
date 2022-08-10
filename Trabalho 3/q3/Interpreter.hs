e5d7d780fee31ba208f36b0b52a54ffd5bea51b1
dudaholandah <duda.mholanda@gmail.com>
2022-08-10 13:34:53 -0300

    add q3

--- Trabalho 3/q3/Interpreter.hs
+++ Trabalho 3/q3/Interpreter.hs
@@ -6,55 +6,54 @@ import Prelude hiding (lookup)
 type RContext = [(String, Integer)]
 type ErrorMessage = String
 
-{- Dica: somente o tipo de executeP precisa mudar (conforme abaixo),
-   mas a sua definicao (corpo) pode continuar a mesma dos exercícios anteriores
--}
 executeP :: RContext -> Program -> Either ErrorMessage RContext
--- executeP context (Prog stm) = execute context stm
-executeP = undefined 
+executeP context (Prog stm) = execute context stm
 
-{- Dica: o tipo de execute deve mudar para
- execute :: RContext -> Stm -> Either ErrorMessage RContext
- Alem disso, o corpo dessa funcao deve ser modificado, pois eval
- retorna tipo diferente agora, ou seja, a avaliacao pode falhar
- e, consequentemente o execute tambem. Assim, todos tipos de comandos
- serao afetados
--}
-execute :: RContext -> Stm -> RContext
+-- change return from RContext to Either Error Message RContext
+execute :: RContext -> Stm -> Either ErrorMessage RContext
 execute context x = case x of
-  SAss id exp -> update context (getStr id) (eval context exp)
-  SBlock [] -> context
-  SBlock (s : stms) -> execute (execute context s) (SBlock stms)
-  SWhile exp stm ->
-    if ((eval context exp) /= 0)
-      then execute (execute context stm) (SWhile exp stm)
-      else context
+  SAss id exp -> case eval context exp of
+                        Right vexp -> Right ( update context (getStr id) vexp )
+                        Left msg -> Left msg
+  SBlock [] -> Right context
+  SBlock (s : stms) -> case execute context s of
+                        Right cont -> execute cont (SBlock stms)
+                        Left msg -> Left msg
+  SWhile exp stm -> case eval context exp of
+                        Right vexp -> if (vexp == 0)
+                                        then Right context
+                                        else case execute context stm of
+                                                Right cont -> execute cont (SWhile exp stm)
+                                                Left msg -> Left msg
+                        Left msg -> Left msg
 
-{- Dica: o tipo de eval deve mudar para
- eval :: RContext -> Exp -> Either ErrorMessage Integer
--}
-eval :: RContext -> Exp -> Integer
+-- change return from Integer to Either Error Message Integer
+eval :: RContext -> Exp -> Either ErrorMessage Integer
 eval context x = case x of
-  EAdd exp0 exp -> eval context exp0 + eval context exp
-  ESub exp0 exp -> eval context exp0 - eval context exp
-  EMul exp0 exp -> eval context exp0 * eval context exp
-  EDiv exp0 exp -> eval context exp0 `div` eval context exp
-  EInt n -> n
-  EVar id -> lookup context (getStr id)
-{-  algumas dicas abaixo...para voce adaptar o codigo acima
-    EDiv e1 e2 -> case eval context e1 of
-                    Right ve1 -> case eval context e2 of
-                                   Right ve2 -> if (ve2 == 0)
-                                                 then Left ("divisao por 0 na expressao: "
-                                                            ++ show (EDiv e1 e2))
-                                                 else Right (ve1 `div` ve2)
-                                  Left msg -> Left msg
-                    Left msg -> Left msg
-    EInt n  ->  Right n
--}
-
-
--- Dica: voce nao precisa mudar o codigo a partir daqui
+  EAdd exp0 exp -> case eval context exp0 of
+                        Right vexp0 -> case eval context exp of
+                                        Right vexp -> Right (vexp0 + vexp)
+                                        Left msg -> Left msg
+                        Left msg -> Left msg
+  ESub exp0 exp -> case eval context exp0 of
+                        Right vexp0 -> case eval context exp of
+                                        Right vexp -> Right (vexp0 - vexp)
+                                        Left msg -> Left msg
+                        Left msg -> Left msg                     
+  EMul exp0 exp -> case eval context exp0 of
+                        Right vexp0 -> case eval context exp of
+                                        Right vexp -> Right (vexp0 * vexp)
+                                        Left msg -> Left msg
+                        Left msg -> Left msg
+  EDiv exp0 exp -> case eval context exp0 of
+                        Right vexp0 -> case eval context exp of
+                                        Right vexp -> if (vexp == 0)
+                                                        then Left ("divisao por 0")
+                                                        else Right (vexp0 `div` vexp)
+                                        Left msg -> Left msg
+                        Left msg -> Left msg
+  EInt n -> Right n
+  EVar id -> Right ( lookup context (getStr id) )
 
 getStr :: Ident -> String
 getStr (Ident s) = s
